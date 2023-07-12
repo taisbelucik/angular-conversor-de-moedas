f2dca66c82dd935e79a400ac7bea157a5af8b3c7
taisbelucik <taisbelucik@hotmail.com>
2023-07-12 15:38:57 -0300

    adicionando validação para somente números

--- src/app/conversor/conversor.module.ts
+++ src/app/conversor/conversor.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { ConversorComponent } from './components';
 import { MoedaService, ConversorService } from './services';
-import { HttpClientModule } from '@angular/common/http';
+import { NumeroDirective } from './directives';
 
 @NgModule({
-  declarations: [ConversorComponent],
-  imports: [CommonModule, HttpClientModule],
+  declarations: [ConversorComponent, NumeroDirective],
+  imports: [CommonModule, HttpClientModule, FormsModule],
   exports: [ConversorComponent],
   providers: [MoedaService, ConversorService],
 })
